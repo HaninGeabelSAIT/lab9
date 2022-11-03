fb91567051f30ce9fa4af2929368dc9aa85118d5
HaninGeabel <haneen4husseen@gmail.com>
2022-11-03 15:40:23 -0600

    Removed the connectionPool class and updated the UBUtil

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -4,30 +4,14 @@
  * and open the template in the editor.
  */
 package dataaccess;
-import java.sql.*; 
-/**
- *
- * @author Hanin
- */
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DBUtil {
-    public static void closePreparedStatement (Statement ps){
-        try {
-            if (ps != null){
-                ps.close(); 
-            }
-        }catch (SQLException e){
-            System.out.println (e); 
-        }
-    }
-    public static void closeResultSet(ResultSet rs){
-        try {
-            if (rs != null){
-                rs.close(); 
-                
-            }
-            
-        }catch (SQLException e){
-            System.out.println (e); 
-        }
+    private static final EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("UsersPU");
+
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
     }
 }
